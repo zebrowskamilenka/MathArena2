8117ea9c8994b8fd1443d629747b1a878f407f4f
zebrowskamilenka <zebrowskamilenka@gmail.com>
2025-12-02 00:05:57 +0100

    dodałam dzialania mateamtyczne w easyactivity java

--- app/src/main/java/com/example/matharena2/DifficultyActivity.java
+++ app/src/main/java/com/example/matharena2/DifficultyActivity.java
@@ -1,6 +1,10 @@
 package com.example.matharena2;
 
+import android.content.Intent;
 import android.os.Bundle;
+ import android.widget.Button;
+import android.widget.ImageButton;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class DifficultyActivity extends AppCompatActivity {
@@ -9,5 +13,12 @@ public class DifficultyActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_difficulty);
+
+        ImageButton easyButton = findViewById(R.id.btnEasy);
+
+        easyButton.setOnClickListener(v -> {
+            Intent intent = new Intent(DifficultyActivity.this, EasyGameActivity.class);
+            startActivity(intent);
+        });
     }
 }
