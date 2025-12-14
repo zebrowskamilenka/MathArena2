c2378a1b16222f91e3ad3fc0adf2fa339faf7522
zebrowskamilenka <zebrowskamilenka@gmail.com>
2025-12-14 17:15:52 +0100

    stworzona klasa mediumgame i layout oraz w easygame.java zostało stworzona logika mniej wiecej

--- app/src/main/java/com/example/matharena2/EasyGameActivity.java
+++ app/src/main/java/com/example/matharena2/EasyGameActivity.java
@@ -1,8 +1,11 @@
 package com.example.matharena2; // dostosuj do swojego pakietu
 
 import android.os.Bundle;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -14,14 +17,23 @@ public class EasyGameActivity extends AppCompatActivity {
     private TextView textLevelTitle;
     private TextView textTask;
     private TextView textTimer;
-    private TextView textScore;
+    private EditText editAnswer;
+    private Button btnOk;
+
+
+    // poprwana odpowiedz i punkty
+    private int correctAnswer = 0;
+    private int points = 0;
+
 
     // BAZA potworków dla ŁATWEGO poziomu
     private final int[] easyMonsters = {
-            R.drawable.p1,
-            R.drawable.p2,
-            R.drawable.p7,
-            R.drawable.p4
+            R.drawable.p1e,
+            R.drawable.p2e,
+            R.drawable.p5e,
+            R.drawable.p8e,
+            R.drawable.p10e,
+            R.drawable.p21e
     };
 
     @Override
@@ -30,13 +42,11 @@ public class EasyGameActivity extends AppCompatActivity {
         setContentView(R.layout.activity_easy_game);
 
         imageMonsterEasy = findViewById(R.id.imageMonsterEasy);
-        textLevelTitle = findViewById(R.id.textLevelTitle);
         textTask = findViewById(R.id.textTask);
         textTimer = findViewById(R.id.textTimer);
-        textScore = findViewById(R.id.textScore);
+        editAnswer = findViewById( R.id.editAnswer);
+        btnOk = findViewById(R.id.btnOk);
 
-        // Ustaw nagłówek - jakby kiedyś było więcej info
-        textLevelTitle.setText("Poziom: Łatwy");
 
         // 1) Losujemy potworka z puli ŁATWYCH
         showRandomEasyMonster();
@@ -46,6 +56,9 @@ public class EasyGameActivity extends AppCompatActivity {
 
         // 3) Ustawiamy mechanizm czasu (na razie też placeholder)
         setupEasyTimer();
+
+        // klikniecie ok -> sprawdza odpowiedz
+        btnOk.setOnClickListener(v -> checkAnswer());
     }
 
     private void showRandomEasyMonster() {
@@ -58,23 +71,54 @@ public class EasyGameActivity extends AppCompatActivity {
     private void generateEasyTask() {
         Random random = new Random();
 
-        //losowanie dwoch liczb z zakresu 1-10
-        int a = random.nextInt(10)+ 1;
-        int b = random.nextInt(10)+1;
+        //losowanie dwoch liczb z zakresu 1-20
+        int a = random.nextInt(20)+ 1;
+        int b = random.nextInt(20) + 1;
 
         // obliczanie wyniku
-        int result = a + b;
+         correctAnswer = a + b;
 
-        String taskText = a + " + " + b + " =? ";
+        String taskText = a + " + " + b + " =  ";
 
         // wyświetlanie na ekraknie
         textTask.setText(taskText);
 
     }
 
+    private void checkAnswer(){
+        String txt = editAnswer.getText().toString().trim();
+
+        if(txt.isEmpty()){
+            Toast.makeText(this, "Wpisz odpowiedź", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        int userAnswer;
+        try{
+            userAnswer = Integer.parseInt(txt);
+        } catch (NumberFormatException e) {
+            Toast.makeText(this, "Podaj liczbę.", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if (userAnswer == correctAnswer) {
+            points += 1;
+            Toast.makeText(this, "Dobrze! Punkty: " + points, Toast.LENGTH_SHORT).show();
+
+            // opcjonalnie: po poprawnej odpowiedzi losuj nowego potworka
+            showRandomEasyMonster();
+        } else {
+            Toast.makeText(this, "Źle! Poprawnie: " + correctAnswer, Toast.LENGTH_SHORT).show();
+        }
+
+        //czyszczenie pola i losowe nowe zadanie
+        editAnswer.setText(" ");
+        generateEasyTask();
+    }
+
     private void setupEasyTimer() {
         // TU KIEDYŚ: logika timera (np. 10 sekund na odpowiedź)
         // Na razie zostawiamy na sztywno:
-        textTimer.setText("Czas: 00:10");
+        textTimer.setText("Time: 00:10");
     }
 }
