1db40589eae9095123c1a367f40081b8c3af2c8f
zebrowskamilenka <zebrowskamilenka@gmail.com>
2026-01-11 16:29:13 +0100

    dodawanie komentarzy cd

--- app/src/main/java/com/example/matharena2/MainActivity.java
+++ app/src/main/java/com/example/matharena2/MainActivity.java
@@ -1,27 +1,27 @@
 package com.example.matharena2;
 
-import android.content.Intent;
+import android.content.Intent; // przechodzi pomiedzy ekranami
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
+import android.view.View; // klasa dla elemntow ui
+import android.widget.Button; //przyciski
 
-import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity; // zapewnienie zgodnosci z starszymi androidami
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main); //ustawienie layoutu z ktorego ma pobrac sie pierwsza strona
 
-        Button startButton = findViewById(R.id.startButton);
-
+        Button startButton = findViewById(R.id.startButton); // pobranie przycisku
+// gdy gracz klika start wykonuje sie kod ponizej
         startButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent =
-                        new Intent(MainActivity.this, DifficultyActivity.class);
-                startActivity(intent);
+                        new Intent(MainActivity.this, DifficultyActivity.class); // przechodzenie z strony do strony difficulty
+                startActivity(intent); // urochomienie kolejnej strony
             }
         });
     }
